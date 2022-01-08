261659fa7df7daf06fb813e28bc777fc9ac03b37
Chaitanya Tyagi <chaitanyatyagi1540@gmail.com>
2022-01-08 20:17:21 +0530

    data changed

---
+++ src/components/Cards.jsx
@@ -0,0 +1,134 @@
+import "../styles/cards.scss";
+import Stories from "./Stories";
+import Card from "./Card";
+
+function Cards() {
+  const commentsOne = [
+    {
+      user: "bunnie",
+      text: "Woah dude, this is awesome! 🔥",
+      id: 1,
+    },
+    {
+      user: "crazy_boy",
+      text: "Like!",
+      id: 2,
+    },
+    {
+      user: "rohan12",
+      text: "Niceeeee!",
+      id: 3,
+    },
+  ];
+
+  const commentsTwo = [
+    {
+      user: "_rahul_",
+      text: "Amazing content, keep it up!",
+      id: 4,
+    },
+  ];
+
+  const commentsThree = [
+    {
+      user: "_AbHi__",
+      text: "Love this!",
+      id: 5,
+    },
+  ];
+  
+  const commentsFour = [
+    {
+      user: "_AbHi__",
+      text: "Lovely mann!",
+      id: 5,
+    },
+  ];
+
+  const commentsFive = [
+    {
+      user: "chetan234",
+      text: "Awsmm!",
+      id: 5,
+    },
+  ];
+
+  const commentsSix = [
+    {
+      user: "rakesh",
+      text: "bravo !!",
+      id: 5,
+    },
+  ];
+
+  return (
+    <div className="cards">
+      <Stories />
+
+      <Card
+        accountName="rishita_123"
+        storyBorder={true}
+        image="https://picsum.photos/800/900"
+        comments={commentsOne}
+        likedByText="roshan122"
+        likedByNumber={89}
+        hours={16}
+      />
+      <Card
+        accountName="caesar"
+        image="https://picsum.photos/800"
+        comments={commentsTwo}
+        likedByText="ritika"
+        likedByNumber={47}
+        hours={12}
+      />
+      <Card
+        accountName="kush"
+        storyBorder={true}
+        image="https://picsum.photos/800/1000"
+        comments={commentsThree}
+        likedByText="luv_1540"
+        likedByNumber={90}
+        hours={4}
+      />
+      <Card
+        accountName="pawan"
+        storyBorder={true}
+        image="https://picsum.photos/800/1000"
+        comments={commentsFour}
+        likedByText="hommie"
+        likedByNumber={900}
+        hours={7}
+      />
+      <Card
+        accountName="hhh__34"
+        storyBorder={true}
+        image="https://picsum.photos/800/1000"
+        comments={commentsFive}
+        likedByText="shuchu"
+        likedByNumber={40}
+        hours={1}
+      />
+      <Card
+        accountName="prachi"
+        storyBorder={true}
+        image="https://picsum.photos/800/1000"
+        comments={commentsSix}
+        likedByText="luv_1540"
+        likedByNumber={190}
+        hours={14}
+      />
+      <Card
+        accountName="chetan"
+        storyBorder={true}
+        image="https://picsum.photos/800/1000"
+        // comments={commentsThree}
+        likedByText="lavi"
+        likedByNumber={132}
+        hours={2}
+      />
+    </div>
+  );
+}
+
+export default Cards;
---
+++ src/components/Suggestions.jsx
@@ -0,0 +1,48 @@
+import "../styles/suggestions.scss";
+import Profile from "./Profile";
+
+function Suggestions() {
+  return (
+    <div className="suggestions">
+      <div className="titleContainer">
+        <div className="title">Suggestions For You</div>
+        <a href="/">See All</a>
+      </div>
+
+      <Profile
+        caption="Followed by rahul_67 + 3 more"
+        urlText="Follow"
+        iconSize="medium"
+        captionSize="small"
+        storyBorder={true}
+      />
+      <Profile
+        caption="Followed by ashu12 + 1 more"
+        urlText="Follow"
+        iconSize="medium"
+        captionSize="small"
+      />
+      <Profile
+        caption="Follows you"
+        urlText="Follow"
+        iconSize="medium"
+        captionSize="small"
+      />
+      <Profile
+        caption="Followed by bunnie + 7 more"
+        urlText="Follow"
+        iconSize="medium"
+        captionSize="small"
+        storyBorder={true}
+      />
+      <Profile
+        caption="Followed by techakhil + 4 more"
+        urlText="Follow"
+        iconSize="medium"
+        captionSize="small"
+      />
+    </div>
+  );
+}
+
+export default Suggestions;
